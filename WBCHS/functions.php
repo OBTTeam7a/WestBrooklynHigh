88b47b3bcb7c894f08a6eefcb483ab6b43a7bd42
Pluro <nicholaspeters212@gmail.com>
2017-07-04 23:48:59 -0400

    add feature image support

--- WBCHS/functions.php
+++ WBCHS/functions.php
@@ -3,7 +3,13 @@
         wp_enqueue_style('style', get_stylesheet_uri());
     }
     add_action('wp)enqueue)scripts', 'wbchs_resources');
+	//theme setup
+	function bannerPic(){
     // Navigation Menus
     register_nav_menus(array(
         'prime'=>__('Primary Menu')
     ));
+	// featured Image
+	add_theme_support('post-tumbnails');
+}
+add_action('after_setup_theme', 'bannerPic')
